0b2ffbd50dc08a6f296ab0b32d7f545406c4d1cd
fluteijn utrecht <frans.luteijn@utrecht.nl>
2024-07-17 17:03:13 +0000

    Build werkend.

--- src/main/kotlin/com/ritense/valtimo/digitaalklantdossierauthenticatie/DigitaalKlantDossierPlugin.kt
+++ src/main/kotlin/com/ritense/valtimo/digitaalklantdossierauthenticatie/DigitaalKlantDossierPlugin.kt
@@ -1,10 +1,21 @@
 package com.ritense.valtimo.digitaalklantdossierauthenticatie
 
-import com.ritense.plugin.PluginFactory
-import com.ritense.plugin.service.PluginService
+import com.ritense.plugin.annotation.Plugin
+import com.ritense.plugin.annotation.PluginProperty
+import java.net.URI
 
-class DigitaalKlantDossierPluginFactory(
-    pluginService: PluginService,
-) : PluginFactory<DigitaalKlantDossierPlugin>(pluginService) {
-    override fun create(): DigitaalKlantDossierPlugin = DigitaalKlantDossierPlugin()
+@Plugin(
+    key = "digitaalklantdossier",
+    title = "Digitaal Klant Dossier",
+    description = "Connects to DKD via OpenTunnel",
+)
+class DigitaalKlantDossierPlugin {
+    @PluginProperty(key = "url", secret = false)
+    lateinit var url: URI
+
+    @PluginProperty(key = "username", secret = false)
+    lateinit var username: String
+
+    @PluginProperty(key = "password", secret = true)
+    lateinit var password: String
 }
